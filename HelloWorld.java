0f7c4ab5d6430fc013cbf21005dbcea12a622203
Parth <parthvijaypatel@gmail.com>
2021-09-27 18:06:50 +0530

    Day 0: Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(name);
+
+        scanner.close();
+    }
+}
